d055f99d73373b0ec80922b0fd6fdb2ad6c68083
Uchenna Igwe <ig.ultrabix@gmail.com>
2025-11-19 08:22:14 +0200

    removed some dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import {
-//   NavigationEnd,
-//   Router,
-//   RouterOutlet,
-//   Event as RouterEvent,
-// } from '@angular/router';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet],
-//   template: ` <router-outlet /> `,
-// })
-// export class AppComponent implements OnInit {
-//   constructor(private router: Router) {}
-
-//   ngOnInit() {
-//     this.router.events.subscribe((event: RouterEvent) => {
-//       if (event instanceof NavigationEnd) {
-//         setTimeout(() => {
-//           // Check if HSStaticMethods exists before calling
-//           if (window.HSStaticMethods) {
-//             window.HSStaticMethods.autoInit();
-//           }
-//         }, 100);
-//       }
-//     });
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
 import {
   NavigationEnd,
